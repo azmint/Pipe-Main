e40c1ed28444b49d299dfb547acf3bca7fbdc42d
Yuki Roppongi <az0.mint9@gmail.com>
2019-01-17 23:09:07 +0900

    [Update] メッセージに発信者Idを追加

--- src/main/java/jp/personal/gi/pipe_main/core/models/message/Message.java
+++ src/main/java/jp/personal/gi/pipe_main/core/models/message/Message.java
@@ -5,11 +5,13 @@ import jp.personal.gi.pipe_main.core.support.entity.Entity;
 
 public class Message extends Entity<MessageId> {
     private final TalkRoomId talkRoomId;
+    private final SenderId senderId;
     private final Text text;
 
-    public Message(MessageId id, TalkRoomId talkRoomId, Text text) {
+    public Message(MessageId id, TalkRoomId talkRoomId, SenderId senderId, Text text) {
         super(id);
         this.talkRoomId = talkRoomId;
+        this.senderId = senderId;
         this.text = text;
     }
 
@@ -17,11 +19,20 @@ public class Message extends Entity<MessageId> {
         return talkRoomId;
     }
 
+    public SenderId getSenderId() {
+        return senderId;
+    }
+
+    public Text getText() {
+        return text;
+    }
+
     @Override
     public String toString() {
         return "Message{" +
                 "id=" + id +
                 ", talkRoomId=" + talkRoomId +
+                ", senderId=" + senderId +
                 ", text=" + text +
                 '}';
     }

5a452de41daba1152f624662f944281077e7b7c1
Yuki Roppongi <az0.mint9@gmail.com>
2019-09-07 14:49:33 +0900

    Specificationのユニットテストを追加

---
+++ src/test/java/jp/personal/gi/pipe_main/domain/support/SpecificationTest.java
@@ -0,0 +1,78 @@
+package jp.personal.gi.pipe_main.domain.support;
+
+import org.junit.*;
+
+import java.util.*;
+import java.util.function.*;
+
+import static org.junit.Assert.*;
+
+public class SpecificationTest {
+
+    @Test
+    public void notSatisfiedBy_inCaseOfTrue() {
+        Specification<String> instance = "AAA"::equals;
+        assertTrue(instance.notSatisfiedBy("aaa"));
+    }
+
+    @Test
+    public void notSatisfiedBy_inCaseOfFalse() {
+        Specification<String> instance = "AAA"::equals;
+        assertFalse(instance.notSatisfiedBy("AAA"));
+    }
+
+    @Test
+    public void not() {
+        Specification<String> instance = "AAA"::equals;
+        assertFalse(instance.not().isSatisfiedBy("AAA"));
+    }
+
+    @Test
+    public void and() {
+        Specification<String> isAAA = "AAA"::equals;
+        Specification<String> isThreeDigits = arg -> arg.length() == 3;
+        Specification<String> instance = isAAA.and(isThreeDigits);
+        assertTrue(instance.isSatisfiedBy("AAA"));
+    }
+
+    @Test
+    public void andNot() {
+        Specification<String> notLessThanThreeDigits = arg -> !(arg.length() < 3);
+        Specification<String> isOverThanThreeDigits = arg -> arg.length() > 3;
+        Specification<String> instance = notLessThanThreeDigits.andNot(isOverThanThreeDigits);
+        assertTrue(instance.isSatisfiedBy("AAA"));
+    }
+
+    @Test
+    public void or() {
+        {
+            Specification<String> isAAA = "AAA"::equals;
+            Specification<String> isBBB = "BBB"::equals;
+            Specification<String> instance = isAAA.or(isBBB);
+            assertTrue(instance.isSatisfiedBy("AAA"));
+            assertTrue(instance.isSatisfiedBy("BBB"));
+        }
+    }
+
+    @Test
+    public void orNot() {
+        Specification<String> isLessThanThreeDigits = arg -> arg.length() < 3;
+        Specification<String> isOverThanThreeDigits = arg -> arg.length() > 3;
+        Specification<String> instance = isLessThanThreeDigits.orNot(isOverThanThreeDigits);
+        assertTrue(instance.isSatisfiedBy("AAA"));
+    }
+
+    @Test
+    public void toPredicate() {
+        Specification<String> specification = "AAA"::equals;
+        Predicate<String> predicate = specification.toPredicate();
+        assertTrue(predicate.test("AAA"));
+    }
+
+    @Test
+    public void satisfy() {
+        Specification<String> specification = "AAA"::equals;
+        Optional<String> maybeAAA = specification.satisfy("AAA");
+        assertEquals(maybeAAA, Optional.of("AAA"));
+    }
+}

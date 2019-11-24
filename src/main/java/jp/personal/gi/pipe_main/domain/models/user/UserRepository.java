0f17448dcfa4951294d51e9a0b6fedc6c4bf98d7
Yuki Roppongi <az0.mint9@gmail.com>
2019-11-24 14:03:46 +0900

    pipe_mainを削除

--- src/main/java/jp/personal/gi/pipe_main/domain/models/user/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package jp.personal.gi.pipe_main.domain.models.user;
-
-import jp.personal.gi.pipe_main.domain.support.repository.Repository;
-
-public interface UserRepository extends Repository<UserId, User> {
-}
--- src/main/java/jp/personal/gi/pipe_main/domain/support/repository/IdentifierGeneratable.java
+++
@@ -1,7 +0,0 @@
-package jp.personal.gi.pipe_main.domain.support.repository;
-
-import jp.personal.gi.pipe_main.domain.support.entity.EntityId;
-
-public interface IdentifierGeneratable<T extends EntityId> {
-    T nextId();
-}
